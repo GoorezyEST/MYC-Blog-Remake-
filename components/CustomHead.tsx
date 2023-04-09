4898b52fe1883ef72d114e56b1e28202516d58f6
GoorezyEST <goorezyestudios@gmail.com>
2023-04-09 20:32:11 -0300

    Trying to fix headers/favicons in Vercel deployment | Test |

--- components/CustomHead.tsx
+++ components/CustomHead.tsx
@@ -12,35 +12,37 @@ type Props = {
 
 export default function CustomHead({ obj }: Props) {
   return (
-    <Head>
-      <title>{obj.title}</title>
-      <meta name="description" content={obj.description} />
-      <meta property="og:title" content={obj.title} />
-      <meta property="og:description" content={obj.description} />
-      <meta property="og:image" content={obj.image} />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      />
-      <link rel="manifest" href="/site.webmanifest" />
-      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
-      <meta name="msapplication-TileColor" content="#da532c" />
-      <meta name="theme-color" content="#ffffff" />
-    </Head>
+    <>
+      <Head>
+        <title>{obj.title}</title>
+        <meta name="description" content={obj.description} />
+        <meta property="og:title" content={obj.title} />
+        <meta property="og:description" content={obj.description} />
+        <meta property="og:image" content={obj.image} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="favicon-16x16.png"
+        />
+        <link rel="manifest" href="site.webmanifest" />
+        <link rel="mask-icon" href="safari-pinned-tab.svg" color="#000000" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+    </>
   );
 }
